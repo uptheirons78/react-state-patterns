ce128de124d74f2fbe484a0a8d21cc263aa77def
Mauro Bono <bono.mauro@gmail.com>
2019-06-12 12:52:29 +0200

    Updates

---
+++ precache-manifest.2b0298c8f4d2c95cf795dd1e9da079c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2caecf7bce364ccf908129d197facef7",
+    "url": "/react-state-patterns/index.html"
+  },
+  {
+    "revision": "55fcf1a8724e16062c22",
+    "url": "/react-state-patterns/static/css/main.addb1808.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-state-patterns/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "55fcf1a8724e16062c22",
+    "url": "/react-state-patterns/static/js/main.36f6d9e8.chunk.js"
+  },
+  {
+    "revision": "2b910e0b9e3dd8cb8a56",
+    "url": "/react-state-patterns/static/js/runtime~main.ca3892fc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-state-patterns/precache-manifest.2b0298c8f4d2c95cf795dd1e9da079c6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-state-patterns/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
